c79adeecbda5b48a5e0e0b9facf36e342d52dc81
darpasoft <igorssac@gmail.com>
2020-07-19 15:38:00 +0200

    Update FleetmanRegistryApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanRegistryApplication.java
@@ -1,3 +1,18 @@
+package com.virtualpairprogrammers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.netflix.appinfo.AmazonInfo;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class FleetmanRegistryApplication {
 
         @Value("${server.port}")
